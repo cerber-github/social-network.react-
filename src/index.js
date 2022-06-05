4c88d4b3521076536976209611852f11f2e6bcdf
cerber-github <berlous.sergiy@ukr.net>
2022-06-05 18:40:57 +0300

    reducer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from "./redux/state";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = (state) => {
