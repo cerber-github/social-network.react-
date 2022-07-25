0a0098577539b9e82911e8095e2b96622be11eff
cerber-github <berlous.sergiy@ukr.net>
2022-07-25 23:41:34 +0300

    setState, local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from "../../Common/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -13,6 +14,7 @@ const ProfileInfo = (props) => {
                 <img src='https://st4.depositphotos.com/20524830/i/600/depositphotos_257969776-stock-photo-wide-panorama-beautiful-sunrise-landscape.jpg'/>
             </div>
             <div className={s.descriptionBlock}>
+               <ProfileStatus status={"Hello"}/> <br/>
                <img src={props.profile.photos.large} />
                 ava
             </div>
